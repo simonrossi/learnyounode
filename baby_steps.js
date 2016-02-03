8dbd89530e80ea07df323e8b93c6dcc1e68f6678
Gary Anderson <ganderson@seattleacademy>
2016-02-03 04:43:27 -0800

    add baby_steps.js template

---
+++ baby_steps.js
@@ -0,0 +1,4 @@
+ ## BABY STEPS (Exercise 2 of 13)  
+   
+  Write a program that accepts one or more numbers as command-line arguments  
+  and prints the sum of those numbers to the console (stdout).  
